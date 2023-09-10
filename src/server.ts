dd78cf47570d0dc20d7c3e3b296bb6d9e7cc8eae
VMarsola <vinicius.marasola2@gmail.com>
2023-09-09 21:15:04 -0300

    init

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import express, { Request, Response } from "express";
+import shortid from "shortid";
+
+const app = express();
+// Usar JSON no corpo das requisições
+app.use(express.json());
+
+// Dicionário para mapear URLs encurtadas para URLs originais
+const urlMapping: { [key: string]: string } = {};
+
+// Endpoint para encurtar URLs
+app.post("/encurtar", (req: Request, res: Response) => {
+  const urlLonga: string = req.body.url;
+  const urlCurta: string = shortid.generate();
+
+  // Armazenar a URL longa no dicionário de mapeamento
+  urlMapping[urlCurta] = urlLonga;
+
+  res.json({ urlCurta: `${req.protocol}://${req.get("host")}/${urlCurta}` });
+});
+
+// Endpoint para redirecionar URLs encurtadas
+app.get("/:urlCurta", (req: Request, res: Response) => {
+  const urlCurta: string = req.params.urlCurta;
+  if (urlCurta in urlMapping) {
+    const urlLonga: string = urlMapping[urlCurta];
+    res.redirect(urlLonga);
+  } else {
+    res.status(404).json({ error: "URL curta não encontrada" });
+  }
+});
